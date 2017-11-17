64135b8fcca7f6928a533a9601f1a217bf0f3360
Mithun Aggarwal <aggarwalmithun@gmail.com>
2017-11-17 12:25:50 +1100

    Commenting done- Restore model removed temp.

--- train.py
+++ train.py
@@ -1,18 +1,23 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Oct 28 10:52:31 2017
+Created on Fri Oct 13 22:35:32 2017
 
-@author: MIT
+@author: magg5201
+For COMP 5703 - Capstone project 
+Unsupervised learning with Gan for LF data
+
+This the the execution file.
+responsible for handling traing and session managemnt
 """
 
+#import modules
 import datetime
 import os
 import sys
-
 import numpy as np
 import tensorflow as tf
 
+#framework modules
 import dcgan
 from config import config
 import utils
@@ -20,6 +25,7 @@ import process_LF
 
 FLAGS = tf.app.flags.FLAGS
 
+#Function creaying various summaries to be displayted on Tensorboard
 def variable_summaries(var):
     """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
     with tf.name_scope('summaries'):
@@ -31,14 +37,10 @@ def variable_summaries(var):
       tf.summary.scalar('max', tf.reduce_max(var))
       tf.summary.scalar('min', tf.reduce_min(var))
       tf.summary.histogram('histogram', var)
-      
+
+#Fuction responsible for training the network
 def train():
-    # placeholder for z
     z = tf.placeholder(tf.float32, [FLAGS.batch_size, FLAGS.z_dim], name='z')
-    # Creates a variable to hold the global_step.
-    #global_step_tensor = tf.Variable(10, trainable=False, name='global_step')
-    #z_samp = tf.placeholder(tf.float32, [FLAGS.Samples_number, FLAGS.z_dim], name='z_samp')
-    # get images
     with tf.name_scope("Input"):
         images,label = dcgan.inputs(batch_size=FLAGS.batch_size)
         with tf.name_scope("Input_image"):
@@ -51,11 +53,13 @@ def train():
             variable_summaries(D_logits_fake)
         with tf.name_scope("D_logits_real"):
             variable_summaries(D_logits_fake)
+    
+    #images to tensorboard    
     with tf.name_scope("Generated_image"):
         tf.summary.image('Generated Image',generated_images,FLAGS.batch_size,\
                          collections=["Generated_image"])
 
-    #images to tensorboard          
+          
     # loss
     with tf.name_scope("loss"):
         d_loss, g_loss, d_loss_real , d_loss_fake = dcgan.loss(D_logits_real, D_logits_fake)
@@ -66,22 +70,23 @@ def train():
         with tf.name_scope("d_loss_real"):
             variable_summaries(d_loss_real)
         with tf.name_scope("d_loss_fake"):
-            variable_summaries(d_loss_fake)            
+            variable_summaries(d_loss_fake) 
+            
     # train the model
     with tf.name_scope("train_ops"):
         train_d_op, train_g_op = dcgan.train(d_loss, g_loss)
             
-    
     #input filter to tensorboard
     dcgan.addfilterstensorboard(FLAGS.num_filters)
     
+    #Variable to be initialized
     init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+    
     #Runtime options
     summary_op = tf.summary.merge_all(key=tf.GraphKeys.SUMMARIES)
     summary_op_Generated_image = tf.summary.merge_all(key="Generated_image")
     summary_op_Input_image = tf.summary.merge_all(key="Input_image")
     summary_op_Generated_filters = tf.summary.merge_all(key="Generated_filters")
-    #summary_op_test_2 = tf.summary.merge_all(key="Test2")
     run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
     run_metadata = tf.RunMetadata()
     
@@ -93,21 +98,19 @@ def train():
     if (FLAGS.restore_model == "True"):
 # =============================================================================
 #             saver = tf.train.Saver()
-#             saver.restore(sess, str("C:/Users/ksxl806/Documents/PythonScripts/LF_DCGAN/LF_DCGAN/checkpoints/LF_DCGAN/model.ckp.index"))
+#             saver.restore(sess, str("~/model.ckp.index"))
 #             print("Model restored.")
 # =============================================================================
         tf.reset_default_graph()  
-        imported_meta = tf.train.import_meta_graph("C:/Users/ksxl806/Documents/PythonScripts/LF_DCGAN/LF_DCGAN/checkpoints/LF_DCGAN_377_547/model.ckp.meta")        
+        #imported_meta = tf.train.import_meta_graph("~/model.ckp.meta")        
     
     with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
         summary_writer = tf.summary.FileWriter(config.LOG_LOCATION,sess.graph)
         sess.run(init)
         threads = tf.train.start_queue_runners(sess=sess,coord = coord)
         
-        if (FLAGS.restore_model == "True"):
-            imported_meta.restore(sess, tf.train.latest_checkpoint('"C:/Users/ksxl806/Documents/PythonScripts/LF_DCGAN/LF_DCGAN/checkpoints/LF_DCGAN_377_547/checkpoint'))
-
-
+        #if (FLAGS.restore_model == "True"):
+            #imported_meta.restore(sess, tf.train.latest_checkpoint("~/LF_DCGAN/checkpoints/LF_DCGAN_377_547/checkpoint'))
         training_steps = FLAGS.train_steps
 
         for step in range(training_steps):
@@ -124,8 +127,6 @@ def train():
             sess.run(train_d_op, feed_dict={z: random_z})
             sess.run(train_g_op, feed_dict={z: random_z})
             sess.run(train_g_op, feed_dict={z: random_z})
-            #print(sess.run(label))
-            
             with tf.name_scope("Loss"):
                 discrimnator_loss, generator_loss = sess.run(
                     [d_loss, g_loss], feed_dict={z: random_z})
@@ -184,17 +185,6 @@ def main(argv=None):
             tf.gfile.DeleteRecursively(FLAGS.output_directory)
         tf.gfile.MakeDirs(FLAGS.output_directory)        
         process_LF._process_dataset("LF", FLAGS.directory_read, FLAGS.num_shards)
-
-# =============================================================================
-#         if tf.gfile.Exists(FLAGS.log_dir):
-#             tf.gfile.DeleteRecursively(FLAGS.log_dir)
-#         tf.gfile.MakeDirs(FLAGS.log_dir)
-#         
-#         if tf.gfile.Exists(FLAGS.checkpoint_dir):
-#             tf.gfile.DeleteRecursively(FLAGS.checkpoint_dir)
-#         tf.gfile.MakeDirs(FLAGS.checkpoint_dir)
-#         train()
-# =============================================================================
         
     if (FLAGS.Train_status == "True"):
         if tf.gfile.Exists(FLAGS.log_dir):
@@ -209,7 +199,5 @@ def main(argv=None):
     else:
         sys.exit()
     
-    #train()
-
 if __name__ == '__main__':
     tf.app.run()
